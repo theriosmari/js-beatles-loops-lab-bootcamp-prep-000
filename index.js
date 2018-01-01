d78bee29cf5245d5d9117186d65be1d0f06b0142
Mariel Ríos <marielbrios09@gmail.com>
2018-01-01 21:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (let i=0; i < 100; i++) {
+  for (let i = 0; i < 100; i++) {
     array.push['musicians[i] plays instruments[i]']
   }
   return array
